0060009e56d6e8a51a7f8221a6fe3f718b17da26
LVandenberghe16 <ludwig.vandenberghe2@gmail.com>
2025-02-27 11:14:42 +0100

    ADD places and test for places features

--- PARITE2_hbnb/app/__init__.py
+++ PARITE2_hbnb/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
 
 
 def create_app():
@@ -13,6 +14,7 @@ def create_app():
     # Additional namespaces for places, reviews, and amenities will be added later
 
     # Enregistrement du namespace des utilisateurs
+    api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
 
--- PARITE2_hbnb/app/services/PlaceFacade.py
+++ PARITE2_hbnb/app/services/PlaceFacade.py
@@ -1,9 +1,18 @@
 from app.models.place import Place
 from app.persistence.repository import InMemoryRepository
+from app.services.AmenityFacade import AmenityFacade
+from app.services.UsersFacade import UsersFacade
+
+
+amenityfacade = AmenityFacade()
+userfacade = UsersFacade()
 
 class PlaceFacade():
+
     def __init__(self):
         self.place_repo = InMemoryRepository()
+        amenityfacade = AmenityFacade()
+        userfacade = UsersFacade()
 
     def create_place(self, place_data):
         place = Place(**place_data)
@@ -11,7 +20,31 @@ class PlaceFacade():
         return place
 
     def get_place(self, place_id):
-        return self.place_repo.get(place_id)
+        place = self.place_repo.get(place_id)
+        if place:
+            owner = userfacade.get_user(place.owner_id)  # Utilisation de la facade pour récupérer l'utilisateur
+            amenities = [amenityfacade.get_amenity(amenity_id) for amenity_id in place.amenities]
+            return {
+                'id': place.id,
+                'title': place.title,
+                'description': place.description,
+                'price': place.price,
+                'latitude': place.latitude,
+                'longitude': place.longitude,
+                'owner': {
+                    'id': owner.id,
+                    'first_name': owner.first_name,
+                    'last_name': owner.last_name,
+                    'email': owner.email
+                } if owner else None,  # Si l'owner n'existe pas, retourner None
+                'amenities': [{
+                    'id': amenity.id,
+                    'name': amenity.name
+                } for amenity in amenities if amenity]  # Filtrer les None si l'amenity n'est pas trouvé
+            }
+        return None
+
+
 
     def get_all_places(self):
         """Récupérer tous les lieux."""
